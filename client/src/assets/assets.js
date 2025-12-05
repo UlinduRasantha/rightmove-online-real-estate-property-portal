9fee01a1c01e9023e5247e5aca245140218b0a4a
UlinduRasantha <rasanthaulindu8888@gmail.com>
2025-12-05 23:56:32 +0530

    create navbar

---
+++ client/src/assets/assets.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import user from './user.png';
+
+export const assets = {
+  logo,
+  user,
+};
+
+export const menuData = [
+            {
+                title: "Buy",
+                links: [
+                    "Property for sale", "Property valuation",
+                    "New homes for sale", "Investors",
+                    "Mortgages"
+                ]
+            },
+            {
+                title: "Rent",
+                links: [
+                    "Property to rent", "Student property to rent",
+                ]
+            },
+            {
+                title: "House Prices",
+                links: ["Sold house prices", "Instant online valuation", "Property valuation"]
+            },
+            {
+                title: "Find Agent",
+                links: ["Find estate agents"]
+            },
+            {
+                title: "Commercial",
+                links: ["Commercial property to rent", "Commercial property for sale", "Advertise commercial property"]
+            },
+            {
+                title: "Inspire",
+                links: ["Moving stories", "Propert news", "Energy efficiency", "Property guides", "Housing trending", 
+                  "Mortage guides", "Overseas blog", "Country guides"]
+            },
+            {
+                title: "Overseas",
+                links: ["All countries", "Spain", "France" , "Portugal", "Italy", "Greece", "Currency", "Sell overseas property"]
+            }
+        ];
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+    // navigate to the diferent routes when needed
+    const navigate = useNavigate();
+
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32'>
+      <img onClick={() => navigate('/')} src={assets.logo} alt="logo" className='w-32 
+      sm:w-44 cursor-pointer' />
+
+      <div>
+        <a href="#">Buy</a>
+      </div>
+
+      <button onClick={() => navigate('/admin')} className='h-10 mr-[10px] bg-white border-2
+       border-primary box-border rounded-[8px] float-left px-4 flex items-center justify-center
+       text-black font-semibold hover:bg-primary hover:text-white transition-colors duration-200'>
+        <img src={assets.user} className='w-3.5 h-3.5 mr-2' alt="" />
+        Sign in
+      </button>
+    </div>
   )
 }
 
